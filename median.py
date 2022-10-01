19ef9cf59a54e23eeabb5db3297a335ea34fc4b2
Erikas Staugas <erikstaugas1@gmail.com>
2022-10-01 15:35:29 +0100

    Print out median

--- median.py
+++ median.py
@@ -1,5 +1,6 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
+import math
 
 while True:
     try:
@@ -9,4 +10,15 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+if(len(numbers) % 2 != 0): # If the length of the list is odd.
+    middleNumber = math.floor(len(numbers) / 2)
+    median = numbers[middleNumber]
+else: # If the length of the list is even.
+    firstMiddleNumber = numbers[int((len(numbers) / 2) - 1)] # Get left hand side of middle.
+    secondMiddleNumber = numbers[int((len(numbers) / 2))] # Get right hand side of middle.
+
+    mean = (firstMiddleNumber + secondMiddleNumber) / 2
+    median = mean
+
+print(median)
